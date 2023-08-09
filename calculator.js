fd994ea18a231c7bee73053e22e394779ceb2928
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-08-09 10:27:23 -0700

    Implement interactive display screen

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
-console.log("success");
+const buttons = document.querySelectorAll('.calculator-container .btn');
+const displayScreen = document.querySelector('#display');
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+    e.preventDefault();
+    changeDisplayScreen(button.textContent);
+    });
+});
+
+
+function changeDisplayScreen(input) {
+    if (input == "C") {
+        displayScreen.textContent = "0";
+        return;
+    }
+    if (input == "AC") {
+        displayScreen.textContent = "0"
+        // Add some more functions to clear history
+        return;
+    }
+    displayScreen.textContent = input;
+}
